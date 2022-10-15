c66009362af0ff89ee597625591d2eb52709a512
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-15 21:21:17 +0500

    ikki02

--- bool02.py
+++ bool02.py
@@ -1,12 +1,10 @@
-def main(a):
-    """
-    Check the logic "The variable 'a' is equal 7"
-    Args:
-        a: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return a==7
+a=7
+b=1
+print(a==b)
+
+
+
+
+
     
 
